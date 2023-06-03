efcd378a791e4f602ef01d94863f2ed74b9c5932
DraggonFantasy <thedraggonfantasy@gmail.com>
2023-06-03 15:37:18 +0300

    Add more chords + add virtual keyboard

--- script.js
+++ script.js
@@ -1,34 +1,90 @@
-function getDegree(key, degree, major, octave) {
-    // Chromatic scale based on sharps
-    const chromaticScale = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
+function getDegree(rootNote, degree, scaleType, octave = 0) {
+    // Whole and half step mapping
+    const WHOLE_STEP = 2;
+    const HALF_STEP = 1;
 
-    // Define step patterns for major and minor scales
-    const stepPattern = major
-        ? [2, 2, 1, 2, 2, 2, 1]  // Major scale
-        : [2, 1, 2, 2, 1, 2, 2]; // Minor scale
+    // Scale patterns
+    const scalePatterns = {
+        'major': [WHOLE_STEP, WHOLE_STEP, HALF_STEP, WHOLE_STEP, WHOLE_STEP, WHOLE_STEP, HALF_STEP],
+        'minor': [WHOLE_STEP, HALF_STEP, WHOLE_STEP, WHOLE_STEP, HALF_STEP, WHOLE_STEP, WHOLE_STEP],
+        'augmented': [WHOLE_STEP, WHOLE_STEP, WHOLE_STEP, HALF_STEP],
+        'diminished': [WHOLE_STEP, HALF_STEP, WHOLE_STEP, HALF_STEP]
+    };
 
-    // Find starting point (root note)
-    let rootNoteIndex = chromaticScale.indexOf(key);
+    // Convert note to MIDI number
+    let midiRootNote = Tone.Frequency(rootNote).toMidi();
+    console.log("midiRootNote", midiRootNote)
 
-    // Calculate the note at the desired degree
+    // Calculate the MIDI number of the degree note
+    let midiDegreeNote = midiRootNote;
     for (let i = 0; i < degree - 1; i++) {
-        rootNoteIndex += stepPattern[i % stepPattern.length];
-        // Wrap around if we go past the end of the chromatic scale
-        if (rootNoteIndex >= chromaticScale.length) {
-            octave += 1;
-            rootNoteIndex -= chromaticScale.length;
-        }
+        midiDegreeNote += scalePatterns[scaleType][i % scalePatterns[scaleType].length];
     }
 
-    // Return the note at the calculated index
-    return [chromaticScale[rootNoteIndex], octave];
+    // Adjust the MIDI number for the desired octave
+    midiDegreeNote += octave * 12;
+    console.log("midiDegreeNote", midiDegreeNote)
+
+    // Convert MIDI number back to note
+    let degreeNote = Tone.Frequency(midiDegreeNote, 'midi').toNote();
+    console.log("degreeNote", degreeNote)
+
+    return degreeNote;
 }
+
+function getChord(rootNote, degree, chordType, scaleType) {
+    // let scale = scaleType === 'major' ? majorScale : minorScale;
+
+    // Get the root note of the chord
+    rootNote += 3;
+    let chordRootNote = getDegree(rootNote, degree, scaleType);
+
+    // Get the third and fifth of the chord
+    let third, fifth;
+
+    switch (chordType) {
+        case 'major':
+            third = getDegree(chordRootNote, 3, 'major', 0);
+            fifth = getDegree(chordRootNote, 5, 'major', 0);
+            break;
+        case 'minor':
+            third = getDegree(chordRootNote, 3, 'minor', 0);
+            fifth = getDegree(chordRootNote, 5, 'major', 0);
+            break;
+        case 'augmented':
+            third = getDegree(chordRootNote, 3, 'major', 0);
+            fifth = getDegree(chordRootNote, 5, 'augmented', 0);
+            break;
+        case 'diminished':
+            third = getDegree(chordRootNote, 3, 'minor', 0);
+            fifth = getDegree(chordRootNote, 5, 'diminished', 0);
+            break;
+    }
+
+    return [chordRootNote, third, fifth];
+}
+
 
 function getPressedKeys(keysPressed) {
     return Object.entries(keysPressed)
         .filter(([key, value]) => value === true)
         .map(([key]) => key);
 }
+
+const keyToDegreeMapping = {
+    '1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '0': 10,
+    'q': 1, 'w': 2, 'e': 3, 'r': 4, 't': 5, 'y': 6, 'u': 7, 'i': 8, 'o': 9,
+    'a': 1, 's': 2, 'd': 3, 'f': 4, 'g': 5, 'h': 6, 'j': 7, 'k': 8, 'l': 9,
+    'z': 1, 'x': 2, 'c': 3, 'v': 4, 'b': 5, 'n': 6, 'm': 7, ',': 8, '.': 9
+};
+
+// Key to chord type mapping
+const keyToChordTypeMapping = {
+    '1': 'major', '2': 'major', '3': 'major', '4': 'major', '5': 'major', '6': 'major', '7': 'major', '8': 'major', '9': 'major', '0': 'major',
+    'q': 'minor', 'w': 'minor', 'e': 'minor', 'r': 'minor', 't': 'minor', 'y': 'minor', 'u': 'minor', 'i': 'minor', 'o': 'minor',
+    'a': 'augmented', 's': 'augmented', 'd': 'augmented', 'f': 'augmented', 'g': 'augmented', 'h': 'augmented', 'j': 'augmented', 'k': 'augmented', 'l': 'augmented',
+    'z': 'diminished', 'x': 'diminished', 'c': 'diminished', 'v': 'diminished', 'b': 'diminished', 'n': 'diminished', 'm': 'diminished', ',': 'diminished', '.': 'diminished'
+};
 
 
 
@@ -52,63 +108,156 @@ document.addEventListener('DOMContentLoaded', function () {
     volume.toDestination();
     let keysPressed = {};  // Object to track which keys are being held down
 
+    window.addEventListener('keydown', (event) => {
+        let key = event.key;
 
-    document.addEventListener('keydown', function (event) {
-        if (event.keyCode >= 48 && event.keyCode <= 57) { // keys 0-9
-            if (keysPressed[event.keyCode]) return;  // Ignore key repeat
-            keysPressed[event.keyCode] = true;  // Mark this key as being held down
+        if (keyToDegreeMapping[key]) {
+            let degree = keyToDegreeMapping[key];
+            let chordType = keyToChordTypeMapping[key];
 
-            let degree = event.keyCode - 48;
-            let scale = event.shiftKey ? 'major' : 'minor';
-            // let [note, octave] = getDegree(currentKey, degree, scale == 'major', 3)
-            let [rootNote, rootOctave] = getDegree(currentKey, degree, scale === 'major', 3);
-            let [thirdNote, thirdOctave] = getDegree(currentKey, degree + 2, scale === 'major', 3); // Get the third degree note
-            let [fifthNote, fifthOctave] = getDegree(currentKey, degree + 4, scale === 'major', 3); // Get the fifth degree note
+            if (!keysPressed[key]) {
+                keysPressed[key] = true;
+                let chordNotes = getChord(currentKey, degree, chordType, 'major'); // assumes we are using major scale
+                synth.triggerAttack(chordNotes);
 
-
-            // console.log(note, octave)
-            rootNote = rootNote + rootOctave;
-            thirdNote = thirdNote + thirdOctave;
-            fifthNote = fifthNote + fifthOctave;
-
-            console.log(rootNote, thirdNote, fifthNote)
-            synth.triggerAttack(rootNote);
-            synth.triggerAttack(thirdNote);
-            synth.triggerAttack(fifthNote);
-
-            // synth.triggerAttack(rootNote, thirdNote, fifthNote);
-
-            console.log(`Key: ${currentKey}, degree: ${degree}, scale: ${scale}`);
+                chordNotes.forEach((n) => {
+                    console.log(n)
+                    const noteKey = document.querySelector(`[data-note="${n}"]`);
+                    noteKey.classList.add('active');
+                })
+            }
         }
     });
 
-    document.addEventListener('keyup', function (event) {
-        if (event.keyCode >= 48 && event.keyCode <= 57) { // keys 0-9
-            if (!keysPressed[event.keyCode]) return;  // Ignore key repeat
-            keysPressed[event.keyCode] = false;  // Mark this key as released
+    // The keyup event
+    window.addEventListener('keyup', (event) => {
+        let key = event.key;
 
-            let degree = event.keyCode - 48;
-            let scale = event.shiftKey ? 'major' : 'minor';
+        if (keyToDegreeMapping[key]) {
+            keysPressed[key] = false;
 
-            // Just an example, you should calculate the actual note based on currentKey, degree, and scale
-            // let [note, octave] = getDegree(currentKey, degree, scale == 'major', 3)
-            let [rootNote, rootOctave] = getDegree(currentKey, degree, scale === 'major', 3);
-            let [thirdNote, thirdOctave] = getDegree(currentKey, degree + 2, scale === 'major', 3); // Get the third degree note
-            let [fifthNote, fifthOctave] = getDegree(currentKey, degree + 4, scale === 'major', 3); // Get the fifth degree note
-            rootNote = rootNote + rootOctave;
-            thirdNote = thirdNote + thirdOctave;
-            fifthNote = fifthNote + fifthOctave;
-
-            // console.log(note, octave)
-            // note = note + octave;
-            // console.log(note, octave)
-
-            // if (Object.values(keysPressed).every(value => value === false)) {
-            // const keys = getPressedKeys()
-            synth.triggerRelease(rootNote);
-            synth.triggerRelease(thirdNote);
-            synth.triggerRelease(fifthNote);
+            // if (!Object.values(keysPressed).some(val => val)) { // if no keys are being pressed
+            //     synth.triggerRelease();
             // }
+            let degree = keyToDegreeMapping[key];
+            let chordType = keyToChordTypeMapping[key];
+            let chordNotes = getChord(currentKey, degree, chordType, 'major'); // assumes we are using major scale
+            synth.triggerRelease(chordNotes);
+            chordNotes.forEach((n) => {
+                const noteKey = document.querySelector(`[data-note="${n}"]`);
+                noteKey.classList.remove('active');
+            })
+
+            //         let [rootNote, rootOctave] = getDegree(currentKey + 3, degree, 'major', 0);
+            //         let [thirdNote, thirdOctave] = getDegree(currentKey + 3, degree + 2, 'major', 0); // Get the third degree note
+            //         let [fifthNote, fifthOctave] = getDegree(currentKey + 3, degree + 4, 'major', 0); // Get the fifth degree note
+            //         rootNote = rootNote + rootOctave;
+            //         thirdNote = thirdNote + thirdOctave;
+            //         fifthNote = fifthNote + fifthOctave;
+
+            //         // console.log(note, octave)
+            //         // note = note + octave;
+            //         // console.log(note, octave)
+
+            //         // if (Object.values(keysPressed).every(value => value === false)) {
+            //         // const keys = getPressedKeys()
+            //         synth.triggerRelease(rootNote);
+            //         synth.triggerRelease(thirdNote);
+            //         synth.triggerRelease(fifthNote);
         }
     });
+
+
+    document.addEventListener('keydown', (event) => {
+        const key = event.key.toUpperCase();
+        const note = document.querySelector(`[data-note="${key}"]`);
+
+        if (note) {
+            note.classList.add('active');
+            // synth.triggerAttack(key + '4');
+        }
+    });
+
+    document.addEventListener('keyup', (event) => {
+        const key = event.key.toUpperCase();
+        const note = document.querySelector(`[data-note="${key}"]`);
+
+        if (note) {
+            note.classList.remove('active');
+            // synth.triggerRelease(key + '4');
+        }
+    });
+
+    // document.addEventListener('keydown', function (event) {
+    //     let key = event.key;
+
+    //     // if (keyToDegreeMapping[key]) {
+    //     //     let degree = keyToDegreeMapping[key];
+    //     //     let chordType = keyToChordTypeMapping[key];
+
+    //     //     if (!keysPressed[key]) {
+    //     //         keysPressed[key] = true;
+    //     //         let chordNotes = getChord(currentKey, degree, chordType, 'major'); // assumes we are using major scale
+    //     //         synth.triggerAttack(chordNotes);
+    //     //     }
+    //     // }
+
+
+    //     if (event.keyCode >= 48 && event.keyCode <= 57) { // keys 0-9
+    //         if (keysPressed[event.keyCode]) return;  // Ignore key repeat
+    //         keysPressed[event.keyCode] = true;  // Mark this key as being held down
+
+    //         let degree = event.keyCode - 48;
+    //         let scale = event.shiftKey ? 'major' : 'minor';
+    //         // let [note, octave] = getDegree(currentKey, degree, scale == 'major', 3)
+    //         let [rootNote, rootOctave] = getDegree(currentKey + 3, degree, 'major', 0);
+    //         let [thirdNote, thirdOctave] = getDegree(currentKey + 3, degree + 2, 'major', 0); // Get the third degree note
+    //         let [fifthNote, fifthOctave] = getDegree(currentKey + 3, degree + 4, 'major', 0); // Get the fifth degree note
+
+
+    //         // console.log(note, octave)
+    //         rootNote = rootNote + rootOctave;
+    //         thirdNote = thirdNote + thirdOctave;
+    //         fifthNote = fifthNote + fifthOctave;
+
+    //         console.log(rootNote, thirdNote, fifthNote)
+    //         synth.triggerAttack(rootNote);
+    //         synth.triggerAttack(thirdNote);
+    //         synth.triggerAttack(fifthNote);
+
+    //         // synth.triggerAttack(rootNote, thirdNote, fifthNote);
+
+    //         console.log(`Key: ${currentKey}, degree: ${degree}, scale: ${scale}`);
+    //     }
+    // });
+
+    // document.addEventListener('keyup', function (event) {
+    //     if (event.keyCode >= 48 && event.keyCode <= 57) { // keys 0-9
+    //         if (!keysPressed[event.keyCode]) return;  // Ignore key repeat
+    //         keysPressed[event.keyCode] = false;  // Mark this key as released
+
+    //         let degree = event.keyCode - 48;
+    //         let scale = event.shiftKey ? 'major' : 'minor';
+
+    //         // Just an example, you should calculate the actual note based on currentKey, degree, and scale
+    //         // let [note, octave] = getDegree(currentKey, degree, scale == 'major', 3)
+    //         let [rootNote, rootOctave] = getDegree(currentKey + 3, degree, 'major', 0);
+    //         let [thirdNote, thirdOctave] = getDegree(currentKey + 3, degree + 2, 'major', 0); // Get the third degree note
+    //         let [fifthNote, fifthOctave] = getDegree(currentKey + 3, degree + 4, 'major', 0); // Get the fifth degree note
+    //         rootNote = rootNote + rootOctave;
+    //         thirdNote = thirdNote + thirdOctave;
+    //         fifthNote = fifthNote + fifthOctave;
+
+    //         // console.log(note, octave)
+    //         // note = note + octave;
+    //         // console.log(note, octave)
+
+    //         // if (Object.values(keysPressed).every(value => value === false)) {
+    //         // const keys = getPressedKeys()
+    //         synth.triggerRelease(rootNote);
+    //         synth.triggerRelease(thirdNote);
+    //         synth.triggerRelease(fifthNote);
+    //         // }
+    //     }
+    // });
 });
